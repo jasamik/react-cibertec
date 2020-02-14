79d87c56e7f8c817f8e1dd320986db16848348ce
jasamik <jasamik@hotmail.com>
2020-02-13 21:42:11 -0500

    Updates

---
+++ precache-manifest.3d5d6a77c6938eb6b686b684805e5c3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b09a011652b297bae61f2e03217ca9",
+    "url": "https//jasamik.github.io/react-cibertec/index.html"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "https//jasamik.github.io/react-cibertec/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "89073398bfc762a9ec12",
+    "url": "https//jasamik.github.io/react-cibertec/static/css/main.12e04618.chunk.css"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "https//jasamik.github.io/react-cibertec/static/js/2.074ebaf5.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "https//jasamik.github.io/react-cibertec/static/js/2.074ebaf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89073398bfc762a9ec12",
+    "url": "https//jasamik.github.io/react-cibertec/static/js/main.06b1412a.chunk.js"
+  },
+  {
+    "revision": "9c48dd505bc6bc758c16",
+    "url": "https//jasamik.github.io/react-cibertec/static/js/runtime-main.aee42a9d.js"
+  },
+  {
+    "revision": "a1e184b8b64d0a5850f0b9a8dad670f8",
+    "url": "https//jasamik.github.io/react-cibertec/static/media/loading.a1e184b8.svg"
+  }
+]);
